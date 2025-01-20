6f9058cafc12d00beca33a54c0c6a099eee20f3b
John <contatocapexdigital@gmail.com>
2025-01-19 22:45:56 -0300

    DB Setting

--- api.py
+++ api.py
@@ -1,7 +1,18 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+class UserModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"User(name = {self.name}, email = {self.name})"
+
 
 @app.route('/')
 def home():
@@ -9,6 +20,7 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
 
 
