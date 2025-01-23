821b9817cac54e9e9f8cc0094f4a00310568052a
John <contatocapexdigital@gmail.com>
2025-01-22 22:29:33 -0300

    Updated

--- api.py
+++ api.py
@@ -153,5 +153,4 @@ def home():
 # Run the application
 if __name__ == '__main__':
     app.run(debug=False)
-
 #End
--- create_db.py
+++ create_db.py
@@ -4,6 +4,5 @@ Initialize FLASK database.
 from api import app, db
 
 with app.app_context():
-    db.create_all()
-    
+    db.create_all()  
 #TheEND
