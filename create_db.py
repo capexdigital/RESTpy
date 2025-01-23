d64c22db5834df0247dd250ac3ad88b6bf81c2cd
John <contatocapexdigital@gmail.com>
2025-01-22 22:37:53 -0300

    Updated

--- create_db.py
+++ create_db.py
@@ -4,6 +4,6 @@ Initialize FLASK database.
 from api import app, db
 
 with app.app_context():
-    db.create_all() 
- 
-#TheEND
+    db.create_all()
+
+# Ensure only one blank line at the end to avoid error C0303(trailing-whitespace)
